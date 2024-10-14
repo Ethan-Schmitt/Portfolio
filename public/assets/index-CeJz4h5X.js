932409655aebd9c5a9ce994148b3207e70551bcc
Ethan-Schmitt <145645263+Ethan-Schmitt@users.noreply.github.com>
2024-10-14 10:47:39 +0200

    changement de certain prblm

---
+++ public/assets/index-CeJz4h5X.js
@@ -0,0 +1,1 @@
+import{r,o as s,c as a,a as l,b as _,F as u}from"./index-fYgMvinG.js";const f=(o,e)=>{const t=o.__vccOpts||o;for(const[n,c]of e)t[n]=c;return t},i={};function m(o,e){const t=r("LoginOut");return s(),a(u,null,[e[0]||(e[0]=l("h1",{class:"text-2xl"},"Accueil",-1)),_(t)],64)}const d=f(i,[["render",m]]);export{d as default};
