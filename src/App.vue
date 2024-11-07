f8898eec2ebafc66ca91c16a23bfcae1f83d745e
Ethan-Schmitt <145645263+Ethan-Schmitt@users.noreply.github.com>
2024-11-07 10:08:01 +0100

    Animation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
+import headers from '@/components/HeaderPage.vue'
 </script>
 
 <template>
+  <headers />
   <header>
     <main>
       <RouterView />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import HeaderPage from '@/components/HeaderPage.vue'
+
 </script>
+<style scoped>
+.full-screen {
+  min-height: 120vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
 <template>
  
- <div class="bg-gradient-to-b from-top-deg to-bot-deg">
-   <HeaderPage />
-  <div class="flex flex-col items-center justify-center min-h-screen p-4 sm:p-8">
-
-      <h1 class="text-3xl md:text-6xl lg:text-7xl text-white font-titre -mb-3">Ethan Schmitt</h1> 
-      <h2 class="text-4xl md:text-7xl lg:text-8xl text-white font-titre">Web développeur</h2>
+ <div class="w-screen w-full animated-background h-screen bg-gradient-to-r from-top-deg to-bot-deg to-mid-deg">
+  <div class="flex flex-col full-screen p-4 sm:p-20">
+      <img src="../components/animations/Anima.gif" alt="GIF animé" class="mb-10 lg:w-24 lg:h-24 md:w-16 md:h-16 w-12 h-12">
+      <h1 class="text-3xl md:text-6xl lg:text-7xl text-white font-titre -mb-4">Ethan Schmitt</h1> 
+      <h2 class="text-4xl md:text-7xl lg:text-8xl text-white font-titre">Web developpeur</h2>
     </div>
-    <LoginOut />
   </div>
 </template>
