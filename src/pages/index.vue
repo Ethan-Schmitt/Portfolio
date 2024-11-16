a53c34172f6daa888be7aac6575d82d07a10e969
Ethan-Schmitt <145645263+Ethan-Schmitt@users.noreply.github.com>
2024-11-16 15:23:18 +0100

    Correction de la police qui présentait des bug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,19 +16,20 @@ import triforce from '@/components/animations/triforces.vue';
     <div class="flex flex-col items-center justify-center min-h-screen p-4 sm:p-8">
       <img src="../components/animations/Anima.gif" alt="GIF animé" class="mb-10 lg:w-24 lg:h-24 md:w-16 md:h-16 w-[65px] h-[61px] mt-20">
       <h1 class="text-4xl md:text-6xl lg:text-7xl text-white font-titre -mb-4">Ethan Schmitt</h1>
-      <h2 class="text-5xl md:text-7xl lg:text-8xl font-titre shine-text">Web developer</h2>
+      <h2 class="text-5xl md:text-7xl lg:text-8xl font-titre shine-text">Web développer</h2>
       <triforce class=" w-[48px] h-[45px] md:w-16 md:h-16 lg:w-65 lg:h-49 absolute -bottom-10 left-0  "  />
     </div>
     <div class="w-2/3 h-1 bg-white justify-center mx-auto mb-10"></div>
-    <h3 class="flex justify-center items-center text-white font-titre md:text-5xl text-4xl">A propos</h3>
+    <h3 class="flex justify-center items-center text-white font-titre md:text-5xl text-3xl">A propos</h3>
     <div class="flex flex-col md:flex-row items-center justify-center space-y-6 md:space-y-0 md:space-x-52 p-4 md:p-12 ">
-      <p class="text-white text-base md:text-lg  font-text text-center md:text-left max-w-3xl">
+      <p class="text-white text-base md:text-lg  font-text text-center md:text-left max-w-3xl ">
         Bienvenue ! Je m’appelle Ethan, j’ai 20 ans et je suis étudiant en deuxième année de BUT Métiers du Multimédia et de l’Internet (MMI), avec une spécialisation en développement web. Passionné par les nouvelles technologies, les jeux vidéo et l’automobile, je suis constamment motivé par de nouveaux défis. Curieux et polyvalent, j’aime explorer et expérimenter avec tout ce qui touche à la technologie, qu’il s’agisse de PC, de tablettes, de téléphones ou de consoles.
         <br><br>
         <em>Pour découvrir davantage sur mon parcours et mes projets, je vous invite à explorer mon site !</em>
       </p>
       <img src="../components/icons/pp.png" alt="Photo de profil" class="w-[130px] h-[130px] md:w-40 md:h-40 lg:w-56 lg:h-56 rounded-full object-cover shadow-lg">
     </div>
+    <h4 class="flex justify-center items-center text-white font-titre md:text-5xl text-3xl md:mt-12">Découvrez mes projet</h4>
   </div>
 </template>
 
